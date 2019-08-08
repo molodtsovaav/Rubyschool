8de778d295eed9e3e353315aa249e3512f1a224c
Belinschi Dima <dmitrii.belinschi@saltedge.com>
2019-08-08 10:34:02 +0300

    show users on index page

--- app.rb
+++ app.rb
@@ -21,7 +21,9 @@ end
 
 
 get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>!!!"
+	# erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>!!!"
+  @users = db.execute( "select * from users" )
+  erb :index
 end
 
 get '/about' do
